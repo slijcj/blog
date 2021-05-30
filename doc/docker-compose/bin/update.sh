653111bed8a9ba36e8b6fba3cdbe95d626834325
slijcj <2834880181@qq.com>
2021-05-30 17:05:01 +0800

    vue_build

--- doc/docker-compose/bin/update.sh
+++ doc/docker-compose/bin/update.sh
@@ -6,25 +6,25 @@ echo '=====开始关闭运行的容器====='
 sh kernShutdown.sh
 
 echo '=====开始更新mogu-gateway====='
-docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/mogu_gateway
+docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/blog/mogu_gateway
 
 echo '=====开始更新mogu-admin====='
-docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/mogu_admin
+docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/blog/mogu_admin
 
 echo '=====开始更新mogu-web====='
-docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/mogu_web
+docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/blog/mogu_web
 
 echo '=====开始更新mogu-sms====='
-docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/mogu_sms
+docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/blog/mogu_sms
 
 echo '=====开始更新mogu-picture====='
-docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/mogu_picture
+docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/blog/mogu_picture
 
 echo '=====开始更新vue_mogu_admin====='
-docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/vue_mogu_admin
+docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/blog/vue_mogu_admin
 
 echo '=====开始更新vue_mogu_web====='
-docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/vue_mogu_web
+docker pull registry.cn-hangzhou.aliyuncs.com/ckpblog/blog/vue_mogu_web
 
 echo '=====删除docker标签为none的镜像====='
 docker images | grep none | awk '{print $3}' | xargs docker rmi
